46a8567a11c13c578f494ae93def2903c5414079
Luis Ruiz <ruiztechservices@gmail.com>
2024-07-22 00:20:36 -0400

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the Product Management API' });
+});
+
+// TODO: Add your routes here
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
